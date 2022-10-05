afc26622a650dbfe20dfb46f60a2dc69c279f5d0
andresev <andresevaldez@gmail.com>
2022-10-05 09:57:37 -0500

    Initial Commit

---
+++ Test-WriteToFile/Test_WriteToFileApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_WriteToFileApp.swift
+//  Test-WriteToFile
+//
+//  Created by Andres Valdez on 10/5/22.
+//
+
+import SwiftUI
+
+@main
+struct Test_WriteToFileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
